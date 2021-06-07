5015425005e2be0bc00a62674d4259fd28b42550
lixiang123395 <55288568+lixiang123395@users.noreply.github.com>
2021-06-07 15:30:36 +0800

    Add files via upload

--- hellogithub.java
+++ hellogithub.java
@@ -2,6 +2,6 @@ package fff;
 
 public class hellogithub {
 	public static void main(String[] args) {
-		System.out.println("hello my github project!");
+		System.out.println("hello my github project!fuck");
 	}
 }
